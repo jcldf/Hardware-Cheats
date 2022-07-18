2774f2d62188665f28e6cb177c2cb772ae6bc9d4
Julio Della Flora <jcldf@hotmail.com>
2022-07-18 15:41:46 -0300

    Update trigger bot com tcs34725 baseado na cor verde.ino

--- trigger bot com tcs34725 baseado na cor verde.ino
+++ trigger bot com tcs34725 baseado na cor verde.ino
@@ -2,62 +2,60 @@
 //isso pode ser utilizado em combinação com outras cores
 
 #include "TCS34725.h"
+
 #include <Mouse.h>
+
 boolean isPressed = false;
 TCS34725 tcs;
 int pinBotao = 16;
 
-    int marcador = 0;
-    int verde = 0;
+int marcador = 0;
+int verde = 0;
 
-void setup(void)
-{
-    Serial.begin(115200);
-    pinMode(pinBotao, INPUT_PULLUP);
-    digitalWrite(pinBotao, LOW);
+void setup(void) {
+  Serial.begin(115200);
+  pinMode(pinBotao, INPUT_PULLUP);
+  digitalWrite(pinBotao, LOW);
 
-    Wire.begin();
-    if (!tcs.attach(Wire))
-        Serial.println("ERROR: TCS34725 NOT FOUND !!!");
+  Wire.begin();
+  if (!tcs.attach(Wire))
+    Serial.println("ERROR: TCS34725 NOT FOUND !!!");
 
-    tcs.integrationTime(15); // ms
+  tcs.integrationTime(15); // ms
 
-    // set LEDs...
-    analogWrite(A0, LOW);
-
+  // set LEDs...
+  analogWrite(A0, LOW);
 
 }
 
-void loop(void)
-{
- 
+void loop(void) {
 
-   while(digitalRead(pinBotao) == HIGH){
-       
+  while (digitalRead(pinBotao) == HIGH) {
+
     if (tcs.available()) // if current measurement has done
     {
-        TCS34725::Color color = tcs.color();
-        if (marcador == 0){
+      TCS34725::Color color = tcs.color();
+      if (marcador == 0) {
         verde = color.g;
-        marcador++;  
-       }
-        int verde2 = color.g;
-       // Serial.print("Red: "); Serial.print(color.r);
+        marcador++;
+      }
+      int verde2 = color.g;
+      // Serial.print("Red: "); Serial.print(color.r);
       //  Serial.print(" Green: "); Serial.print(color.g);
-       // Serial.print(" Blue: "); Serial.println(color.b);  
-        
-       
-          
-          Serial.print(" Green: "); Serial.print(verde2);
-          Serial.print(" CONSTANTE: "); Serial.println(verde);
-          if(verde != verde2){
+      // Serial.print(" Blue: "); Serial.println(color.b);  
 
-           Mouse.click(MOUSE_LEFT);
-          delay(1000);
-            
-          }     
-      
-        }            
+      Serial.print(" Green: ");
+      Serial.print(verde2);
+      Serial.print(" CONSTANTE: ");
+      Serial.println(verde);
+      if (verde != verde2) {
+
+        Mouse.click(MOUSE_LEFT);
+        delay(1000);
+
+      }
+
     }
-    marcador = 0;
+  }
+  marcador = 0;
 }
