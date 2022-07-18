43b4621afa2e0a01db7d086dd28bbd68301df6a8
Julio Della Flora <jcldf@hotmail.com>
2022-07-18 15:30:40 -0300

    Update AimBot baseado em hardware utilizando a Pixy2 e o arduino.ino

--- AimBot baseado em hardware utilizando a Pixy2 e o arduino.ino
+++ AimBot baseado em hardware utilizando a Pixy2 e o arduino.ino
@@ -1,54 +1,53 @@
-//esse aimbot pega posições informadas pela câmera pixy2 e informa essas posições para o arduino leonardo, e necessário notar que a câmera pixy 2 precisa ficar próxima ao monitor em um relação 1 para 1 de pixels
+//esse aimbot pega posições informadas pela câmera pixy2
+//informa essas posições para o arduino leonardo 
+//é necessário notar que a câmera pixy 2 precisa ficar próxima ao monitor em um relação 1 para 1 de pixels
 
 #include <Pixy2.h>
+
 #include <SPI.h>
+
 #include <Mouse.h>
 
-// This is the main Pixy object 
+
 Pixy2 pixy;
 
-
-void setup()
-{
+void setup() {
   Serial.begin(115200);
   Serial.print("Starting...\n");
   Mouse.begin();
   pixy.init();
 }
 
-void loop()
-{ 
-  int i; 
+void loop() {
+  int i;
   // grab blocks!
   pixy.ccc.getBlocks();
-  
+
   // If there are detect blocks, print them!
-  if (pixy.ccc.numBlocks)
-  {
+  if (pixy.ccc.numBlocks) {
     Serial.print("Detected ");
     Serial.println(pixy.ccc.numBlocks);
-    for (i=0; i<pixy.ccc.numBlocks; i++)
-    {
+    for (i = 0; i < pixy.ccc.numBlocks; i++) {
       Serial.print("  block ");
       Serial.print(i);
       Serial.print(": ");
       //pixy.ccc.blocks[i].print();
       int width = pixy.ccc.blocks[i].m_x;
       int height = pixy.ccc.blocks[i].m_y;
-      Serial.print(width); 
-      Serial.print(" "); 
+      Serial.print(width);
+      Serial.print(" ");
       Serial.println(height);
       //int x = map(width, 157, -157, 0, 315);
       //int y = map(height, 103, -103, 0, 207); 
-      
+
       int x = map(width, 0, 315, -157, 157);
       int y = map(height, 0, 207, -103, 103);
-     
+
       Serial.print(x);
       Serial.print(" ");
       Serial.println(y);
       Mouse.move(x, y);
       delay(5);
     }
-  }  
+  }
 }
