ca34bee55958ec3dd7c54fb4a5cdafa43288b7a4
Julio Della Flora <jcldf@hotmail.com>
2022-07-18 15:37:42 -0300

    Update autofire simples com clock usando arduino pro micro.ino

--- autofire simples com clock usando arduino pro micro.ino
+++ autofire simples com clock usando arduino pro micro.ino
@@ -3,46 +3,38 @@
 #include <Mouse.h>
 
 
-
 boolean isPressed = false;
-
 
 int pinBotao = 16;
 
 void setup() {
-pinMode(pinBotao, INPUT_PULLUP);
-digitalWrite(pinBotao, LOW);
- 
+  pinMode(pinBotao, INPUT_PULLUP);
+  digitalWrite(pinBotao, LOW);
 
 }
 
 void loop() {
-label:
-Mouse.release(MOUSE_LEFT);
+  label: Mouse.release(MOUSE_LEFT);
 
+  if (digitalRead(pinBotao) == HIGH) {
+    isPressed = true;
+  } else {
+    isPressed == false;
+  }
 
-if (digitalRead(pinBotao) == HIGH){
-  isPressed = true;
-} else{
-  isPressed == false;
-}
+  if (isPressed == true) {
 
-  
+    Mouse.click(MOUSE_LEFT);
+    delay(20);
 
-if (isPressed == true)
-{
+    Mouse.move(0, 1);
+    delay(20);
+    Mouse.click(MOUSE_LEFT);
+    if (digitalRead(pinBotao) == LOW) {
+      isPressed = false;
+      goto label;
+    }
 
-Mouse.click(MOUSE_LEFT);
-delay(20);
-
-Mouse.move(0, 1); delay(20);
-Mouse.click(MOUSE_LEFT);
-if (digitalRead(pinBotao) == LOW){
-  isPressed = false;
-  goto label;
-}
-
-
-}
+  }
 
 }
